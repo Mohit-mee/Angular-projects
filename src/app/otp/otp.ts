a4a923eb0e61e77849472654c71484998246e4e9
Mohit-mee <itsmohit739@gmail.com>
2026-07-23 23:36:44 +0530

    new changes

--- src/app/otp/otp.ts
+++ src/app/otp/otp.ts
@@ -1,20 +1,31 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, MaxValidator, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-otp',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './otp.html',
   styleUrl: './otp.css',
 })
 export class Otp {
+  userform = new FormGroup({
+    num: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+  })
+  numValue: string = "";
   phoneValue: string = '';
   isCorrect = false;
+  isSent = false;
 
   otp() {
     if (this.phoneValue == '1234') {
       this.isCorrect = true;
       console.log("true")
     }
+  }
+  otpsent() {
+    console.log("otp sent")
+    this.isSent = true;
+    this.numValue = '';
   }
 }
